1f2e6eb28fb96deba9998a130b1997271b8dac41
Art Supreme V <supreme036@gmail.com>
2014-12-09 19:53:55 +0000

    Update gemfile for deployment to Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+  def hello
+    render text: "<h1>Hello</h1><p>Welcome to SupremeMetrics!</p>"
+  end
+end
