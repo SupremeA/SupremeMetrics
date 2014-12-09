093c489260b7180cbf0f7f3bc463b8163faeca9c
Art Supreme V <supreme036@gmail.com>
2014-12-09 19:49:11 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SupremeMetrics::Application.config.secret_key_base = '92b257a14ef9bdb6fb738173a83cb075996450282b5dbfe9e0d97c3019ccc0e695e280c621c84d9b4bfa32ca26adf5a62afdefed50aae04f8b14e55bbd4c2926'
